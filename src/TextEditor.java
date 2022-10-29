ac1be866acb282c6ea2a32e5c312564052ef45b7
hihihaha <hieutedam2k2@gmail.com>
2022-10-29 08:56:29 +0700

    add action open, save, new ,quit

--- src/TextEditor.java
+++ src/TextEditor.java
@@ -1,16 +1,28 @@
+import javax.swing.JFileChooser;
 import javax.swing.JFrame;
 import javax.swing.JMenu;
 import javax.swing.JMenuBar;
 import javax.swing.JMenuItem;
+import javax.swing.JOptionPane;
 import javax.swing.JTextArea;
 import javax.swing.UIManager;
 import javax.swing.UnsupportedLookAndFeelException;
+import javax.swing.filechooser.FileSystemView;
 
 import org.w3c.dom.Text;
 
 import java.awt.event.*;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+
+import java.util.Scanner;
 import java.util.logging.*;
+
+import java.awt.Component;
 
 /**
  * TextEditor
@@ -62,6 +74,63 @@ public class TextEditor extends JFrame implements ActionListener {
     }
 
     public void actionPerformed(ActionEvent e) {
+        String ingest = null;
+        JFileChooser jfc = new JFileChooser(FileSystemView.getFileSystemView().getHomeDirectory());
+        jfc.setDialogTitle("Choose destination.");
+        jfc.setFileSelectionMode(JFileChooser.FILES_AND_DIRECTORIES);
+
+        String ae = e.getActionCommand();
+        switch (ae) {
+            case "Open": {
+                returnValue = jfc.showOpenDialog(null);
+                if (returnValue == JFileChooser.APPROVE_OPTION) {
+                    File f = new File(jfc.getSelectedFile().getAbsolutePath());
+                    try {
+                        FileReader read = new FileReader(f);
+                        Scanner scan = new Scanner(read);
+                        while (scan.hasNextLine()) {
+                            String line = scan.nextLine();
+                            ingest += line;
+                        }
+                        area.setText(ingest);
+                        scan.close();
+                    } catch (FileNotFoundException ex) {
+                        ex.getStackTrace();
+                    }
+                }
+                break;
+            }
+            case "Save": {
+                returnValue = jfc.showSaveDialog(null);
+                try {
+                    File f = new File(jfc.getSelectedFile().getAbsolutePath());
+                    FileWriter out = new FileWriter(f);
+                    out.write(area.getText());
+                    out.close();
+                } catch (FileNotFoundException ex) {
+                    Component f = null;
+                    JOptionPane.showMessageDialog(f, "File not found.");
+
+                } catch (IOException ex) {
+                    Component f = null;
+                    JOptionPane.showMessageDialog(f, "Error");
+                }
+                break;
+            }
+            case "New": {
+                area.setText("");
+                break;
+            }
+            case "Quit": {
+                System.exit(0);
+                break;
+            }
+
+            default:
+                System.exit(0);
+                break;
+        }
 
     }
+
 }
